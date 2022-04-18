10f11d0589af09a760d88c75951fa90b4ab29b6b
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2022-04-18 19:03:19 +0300

    Added new files

--- src/app/ui/place-edit-dialog/place-edit-dialog.module.ts
+++ src/app/ui/place-edit-dialog/place-edit-dialog.module.ts
@@ -1,7 +1,9 @@
+import { HttpClientModule } from "@angular/common/http"
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from "@angular/forms"
 import { MdEditorModule } from "../shared/components/md-editor/md-editor.module"
+import { PhotosUploaderModule } from "../shared/components/photos-uploader/photos-uploader.module"
 import { RatingModule } from "../shared/components/rating/rating.module"
 import { TagsModule } from "../shared/components/tags/tags.module"
 import { PlaceEditComponent } from './components/place-edit/place-edit.component';
@@ -18,7 +20,9 @@ import { PlaceEditComponent } from './components/place-edit/place-edit.component
     RatingModule,
     ReactiveFormsModule,
     TagsModule,
-    MdEditorModule
+    MdEditorModule,
+    PhotosUploaderModule,
+    HttpClientModule
   ]
 })
 export class PlaceEditDialogModule { }
